847247f3a8470797e9f9e006ccc2f701b3977ccb
brs14shn <brsshn292929@gmail.com>
2022-10-11 21:12:06 +0300

    first commit

---
+++ personalApp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Department(models.Model) :
+    name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+class Personal(models.Model):
+    TITLE = (
+        ("Team Lead", "LEAD"),
+        ("Mid Lead", "MID"),
+        ("Junior", "JUN"),
+    )
+
+    GENDER =(
+        ("Female", "F"),
+        ("Male", "M"),
+        ("Other", "O"),
+        ("Prefer Not Say", "N"),
+    )
+    gender = models.CharField(max_length=50, choices=GENDER)
+    title = models.CharField(max_length=50, choices=TITLE)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL,null=True,related_name="departments")
+    #related_name="deparments" bize personellerin deparment ile ilişkisi olduğunu söyler ve daha sonra çağırmak için kullanılır kolaylık sağlar. deparment.id ve personel.id ile ilişkiyi kontrol edebiliriz.#related_name="deparments" bize personellerin deparment ile ilişkisi olduğunu söyler ve daha sonra çağırmak için kullanılır kolaylık sağlar. deparment.id ve personel.id ile ilişkiyi kontrol edebiliriz.
+    create_user = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+    first_name =models.CharField(max_length=50)
+    last_name =models.CharField(max_length=50)
+    is_staffed =models.BooleanField(default=False)
+    title =models.CharField(max_length=50,choices=TITLE)
+    gender=models.CharField(max_length=50,choices=GENDER)
+    salary = models.IntegerField(default=1250)
+    start_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.title}- {self.first_name}"
+    
